708127e05dd5f95734a883c3d6cdb7107b2530cd
Zaid1049 <zaidelmouissia2003@gmail.com>
2025-12-20 18:45:43 +0100

    post-commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./Pages/**/*.{cshtml,cs}",
+        "./Areas/**/*.{cshtml,cs}",
+        "./wwwroot/**/*.{html,js}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
