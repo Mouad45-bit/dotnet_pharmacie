48c81da236722ce6e6b5ed3d98870142ea788aec
mouad fekkak <mouadfekkak2002@gmail.com>
2025-12-20 16:08:19 +0100

    separate the mocks v1 -- Areas/Admin/Services/

--- Areas/Admin/Pages/Personnel/Index.cshtml.cs
+++ Areas/Admin/Pages/Personnel/Index.cshtml.cs
@@ -1,21 +1,14 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using project_pharmacie.Areas.Admin.Services;
 
 namespace project_pharmacie.Areas.Admin.Pages.Personnel;
 
 public class IndexModel : PageModel
 {
-    public record PersonnelRow(string FullName, string Email, string Role, bool IsActive);
-
-    public List<PersonnelRow> Items { get; private set; } = new();
+    public List<Services.Personnel> Items { get; private set; } = new();
 
     public void OnGet()
     {
-        // Mock data juste pour tester l’Area + Tailwind
-        Items = new List<PersonnelRow>
-        {
-            new("Admin Demo", "admin@pharmacie.local", "Admin", true),
-            new("Sara Pharmacien", "sara@pharmacie.local", "Pharmacien", true),
-            new("Yassine Caissier", "yassine@pharmacie.local", "Caissier", false),
-        };
+        Items = PersonnelStore.All();
     }
 }
