59f9591d92252f036b5468276c908a3a787a26ec
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-05 13:02:11 +0100

    update Model/Client v2

--- Models/client.cs
+++ Models/client.cs
@@ -1,5 +1,4 @@
-﻿using Humanizer.Localisation;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace project_pharmacie.Models;
 
@@ -9,7 +8,21 @@ public class Client
     public string Id { get; set; } = Guid.NewGuid().ToString("N");
 
     [Required, MaxLength(120)]
-    public string Nom { get; set; } = "";
+    public string Name { get; set; } = "";
+
+    [MaxLength(180)]
+    public string Email { get; set; } = "";
+
+    [MaxLength(40)]
+    public string Phone { get; set; } = "";
+
+    public int LoyaltyPoints { get; set; } = 0;
+
+    [MaxLength(40)]
+    public string Status { get; set; } = "Nouveau";
+
+    [MaxLength(300)]
+    public string PersonalizedOffer { get; set; } = "";
 
     public ICollection<Vente> Ventes { get; set; } = new List<Vente>();
 }
