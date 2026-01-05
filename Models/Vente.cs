aae13414b06be6250fe9f6951ce0299dfa1daaaf
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-05 12:31:43 +0100

    create Models/Vente v1

---
+++ Models/Vente.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace project_pharmacie.Models;
+
+public class Vente
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString("N");
+
+    public DateTime DateVente { get; set; } = DateTime.Now;
+
+    [Required]
+    public string ClientId { get; set; } = "";
+    public Client? Client { get; set; }
+
+    public ICollection<VenteLigne> Lignes { get; set; } = new List<VenteLigne>();
+
+    public Facture? Facture { get; set; } // 1-1
+}
+
+public class VenteLigne
+{
+    public string VenteId { get; set; } = "";
+    public Vente? Vente { get; set; }
+
+    public string ProduitReference { get; set; } = "";
+    public Produit? Produit { get; set; }
+
+    public int Quantite { get; set; }
+    public decimal PrixUnitaire { get; set; }
+}
