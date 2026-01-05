ac29238e0376f8e8b62c6cb1851ad278d45de430
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-06 00:45:09 +0100

    create Services/ICommandeService.cs v1

---
+++ Services/ICommandeService.cs
@@ -0,0 +1,13 @@
+﻿namespace project_pharmacie.Services;
+
+public interface ICommandeService
+{
+    Task<ServiceResult<OrderCreatedInfo>> CreateAsync(string supplierId, string productRef, int quantity);
+
+    public record OrderCreatedInfo(
+        string OrderId,
+        decimal Total,
+        string SupplierName,
+        string ProductName
+    );
+}
