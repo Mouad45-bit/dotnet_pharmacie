27806583bc5bd25470c8a0266095fe493f894846
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-05 12:28:24 +0100

    create Models/Produit v1

---
+++ Models/Produit.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace project_pharmacie.Models;
+
+public class Produit
+{
+    [Key, MaxLength(50)]
+    public string Reference { get; set; } = "";
+
+    [Required, MaxLength(150)]
+    public string Nom { get; set; } = "";
+
+    public decimal Prix { get; set; }
+    public int Quantite { get; set; }
+    public DateTime? DatePeremption { get; set; }
+
+    // géré par
+    public string? PersonnelId { get; set; }
+    public Personnel? Personnel { get; set; }
+
+    public ICollection<VenteLigne> VenteLignes { get; set; } = new List<VenteLigne>();
+    public ICollection<CommandeLigne> CommandeLignes { get; set; } = new List<CommandeLigne>();
+
+    public ICollection<FournisseurProduit> Fournisseurs { get; set; } = new List<FournisseurProduit>();
+}
