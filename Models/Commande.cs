2314b63a25ce7eab5f5a373a1b1bc2e3c402afe4
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-05 12:35:13 +0100

    create Models/Commande v1

---
+++ Models/Commande.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace project_pharmacie.Models;
+
+public class Commande
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString("N");
+
+    public DateTime Date { get; set; } = DateTime.Now;
+    public double Note { get; set; }
+    public decimal PrixTotal { get; set; }
+
+    [Required]
+    public string FournisseurId { get; set; } = "";
+    public Fournisseur? Fournisseur { get; set; }
+
+    // qui a passé la commande
+    public string? PersonnelId { get; set; }
+    public Personnel? Personnel { get; set; }
+
+    public ICollection<CommandeLigne> Lignes { get; set; } = new List<CommandeLigne>();
+}
+
+public class CommandeLigne
+{
+    public string CommandeId { get; set; } = "";
+    public Commande? Commande { get; set; }
+
+    public string ProduitReference { get; set; } = "";
+    public Produit? Produit { get; set; }
+
+    public int Quantite { get; set; }
+    public decimal PrixUnitaire { get; set; }
+}
