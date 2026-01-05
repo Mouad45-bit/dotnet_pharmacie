f6f44639b4ae6967e7f411cec35cb94bd29f6198
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-05 12:32:31 +0100

    create Models/Facture v1

---
+++ Models/Facture.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace project_pharmacie.Models;
+
+public class Facture
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString("N");
+
+    [Required]
+    public string VenteId { get; set; } = "";
+    public Vente? Vente { get; set; }
+
+    [MaxLength(200)]
+    public string Sujet { get; set; } = "";
+
+    public DateTime DateFacture { get; set; } = DateTime.Now;
+}
