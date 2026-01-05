137aca77d14f71f9e97caf4fb03644daac7f9e6a
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-05 21:49:56 +0100

    create Services/IProduitService.cs v1

---
+++ Services/IProduitService.cs
@@ -0,0 +1,12 @@
+﻿using project_pharmacie.Models;
+
+namespace project_pharmacie.Services;
+
+public interface IProduitService
+{
+    Task<List<Produit>> ListAsync(string? query = null);
+    Task<Produit?> GetByReferenceAsync(string reference);
+
+    // Stock
+    Task<ServiceResult> DecreaseStockAsync(string reference, int quantity);
+}
