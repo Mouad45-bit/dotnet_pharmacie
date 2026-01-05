a83cc6f3b88052bad7b4f6599f4109dd7850d34c
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-05 21:58:26 +0100

    update Services/IProduitService.cs v2

--- Services/IProduitService.cs
+++ Services/IProduitService.cs
@@ -4,8 +4,16 @@ namespace project_pharmacie.Services;
 
 public interface IProduitService
 {
+    // Liste (option recherche)
     Task<List<Produit>> ListAsync(string? query = null);
+
+    // Détail
     Task<Produit?> GetByReferenceAsync(string reference);
+
+    // CRUD (utile pour refactor Pages Products)
+    Task<ServiceResult> CreateAsync(Produit produit);
+    Task<ServiceResult> UpdateAsync(Produit produit);
+    Task<ServiceResult> DeleteAsync(string reference);
 
     // Stock
     Task<ServiceResult> DecreaseStockAsync(string reference, int quantity);
