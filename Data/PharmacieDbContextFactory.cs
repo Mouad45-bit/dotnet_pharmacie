1e81565ac8abb89507a8e50aeb7305da3f8b9b1a
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-05 19:33:38 +0100

    create Data/PharmacieDbContextFactory.cs v1

---
+++ Data/PharmacieDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace project_pharmacie.Data;
+
+public class PharmacieDbContextFactory : IDesignTimeDbContextFactory<PharmacieDbContext>
+{
+    public PharmacieDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<PharmacieDbContext>();
+
+        optionsBuilder.UseSqlServer(
+            "Server=.;Database=PharmacieDb;Trusted_Connection=True;TrustServerCertificate=True"
+        );
+
+        return new PharmacieDbContext(optionsBuilder.Options);
+    }
+}
