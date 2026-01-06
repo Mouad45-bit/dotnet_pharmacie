86c7445eba2646ea1a44bd86e2b7206d81fc81ab
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-06 01:11:22 +0100

    update Program.cs v3.2

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<PharmacieDbContext>(options =>
 
 builder.Services.AddScoped<IProduitService, ProduitService>();
 builder.Services.AddScoped<ICommandeService, CommandeService>();
+builder.Services.AddScoped<IVenteService, VenteService>();
 
 var app = builder.Build();
 
