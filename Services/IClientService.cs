a3b9c711609781c456ab4c393494aaa33b599d7e
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-06 12:30:35 +0100

    create Services/IClientService.cs v1.1

--- Services/IClientService.cs
+++ Services/IClientService.cs
@@ -6,4 +6,7 @@ public interface IClientService
 {
     Task<(List<Client> items, int total)> SearchAsync(string? q);
     Task<ServiceResult> DeleteAsync(string id);
+
+    Task<Client?> GetByIdAsync(string id);
+    Task<ServiceResult> UpdateAsync(Client client);
 }
