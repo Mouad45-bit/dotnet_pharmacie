0b3915064cff66a4e57201bfcc807895972547ca
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-06 13:00:24 +0100

    create Services/IFournisseurService.cs v1.1

--- Services/IFournisseurService.cs
+++ Services/IFournisseurService.cs
@@ -3,4 +3,7 @@
 public interface IFournisseurService
 {
     Task<SupplierListVm> GetDashboardAsync(string? sort);
+
+    Task<SupplierRateVm?> GetRatePageAsync(string supplierId);
+    Task<ServiceResult> RateAsync(string supplierId, int rating, string? comment);
 }
