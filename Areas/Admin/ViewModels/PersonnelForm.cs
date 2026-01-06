64609c422cca88ea86d364242eda97b8d8b12ce4
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-06 13:30:03 +0100

    update Admin/ViewModels/PersonnelForm.cs v2.1

--- Areas/Admin/ViewModels/PersonnelForm.cs
+++ Areas/Admin/ViewModels/PersonnelForm.cs
@@ -1,14 +1,16 @@
-﻿namespace project_pharmacie.Areas.Admin.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
 
-public enum PersonnelPoste
-{
-    Pharmacien,
-    Caissier,
-    Preparateur
-}
+namespace project_pharmacie.Areas.Admin.ViewModels;
 
 public class PersonnelForm
 {
+    [Required(ErrorMessage = "Nom requis")]
     public string Nom { get; set; } = "";
+
+    [Required(ErrorMessage = "Login requis")]
     public string Login { get; set; } = "";
+
+    [Required(ErrorMessage = "Rôle requis")]
+    [RegularExpression("^(ADMIN|PERSONNEL)$", ErrorMessage = "Rôle invalide.")]
+    public string Role { get; set; } = "PERSONNEL";
 }
