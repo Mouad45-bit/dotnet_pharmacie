da3fece384c29de2bd80f5a5a57c6aef8c35d9c7
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-06 23:28:58 +0100

    create Models/ApplicationUser.cs v1

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace project_pharmacie.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    [MaxLength(120)]
+    public string Nom { get; set; } = "";
+}
