e14e647754d3f4fffb684930f342ad009731e396
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-06 09:20:34 +0100

    create Services/IPersonnelService.cs v1

---
+++ Services/IPersonnelService.cs
@@ -0,0 +1,14 @@
+﻿using project_pharmacie.Areas.Admin.ViewModels;
+using project_pharmacie.Models;
+
+namespace project_pharmacie.Services;
+
+public interface IPersonnelService
+{
+    Task<List<Personnel>> SearchAsync(string? q);
+    Task<Personnel?> GetAsync(string id);
+
+    Task<ServiceResult<Personnel>> CreateAsync(PersonnelForm form);
+    Task<ServiceResult> UpdateAsync(string id, PersonnelForm form);
+    Task<ServiceResult> DeleteAsync(string id);
+}
