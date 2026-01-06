af7397d0c92ac02e494fe709d60ece482085c3a9
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-06 12:24:00 +0100

    create Services/IClientService.cs v1

---
+++ Services/IClientService.cs
@@ -0,0 +1,9 @@
+﻿using project_pharmacie.Models;
+
+namespace project_pharmacie.Services;
+
+public interface IClientService
+{
+    Task<(List<Client> items, int total)> SearchAsync(string? q);
+    Task<ServiceResult> DeleteAsync(string id);
+}
