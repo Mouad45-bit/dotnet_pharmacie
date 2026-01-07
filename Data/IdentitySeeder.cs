0b169b43780952b7b38207ad8fd2d1db3b68a878
mouad fekkak <mouadfekkak2002@gmail.com>
2026-01-07 01:02:07 +0100

    create Data/IdentitySeeder.cs v1

---
+++ Data/IdentitySeeder.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using project_pharmacie.Models;
+
+namespace project_pharmacie.Data;
+
+public static class IdentitySeeder
+{
+    public const string AdminRole = "ADMIN";
+    public const string PersonnelRole = "PERSONNEL";
+
+    public static async Task SeedAsync(IServiceProvider services)
+    {
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+        // Roles
+        foreach (var role in new[] { AdminRole, PersonnelRole })
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+                await roleManager.CreateAsync(new IdentityRole(role));
+        }
+
+        // Admin default
+        var adminUsername = "admin";
+        var admin = await userManager.FindByNameAsync(adminUsername);
+
+        if (admin is null)
+        {
+            admin = new ApplicationUser
+            {
+                UserName = adminUsername,
+                Email = "admin@local",
+                EmailConfirmed = true,
+                Nom = "Admin Principal"
+            };
+
+            var create = await userManager.CreateAsync(admin, "Admin@12345");
+            if (create.Succeeded)
+                await userManager.AddToRoleAsync(admin, AdminRole);
+        }
+        else
+        {
+            if (!await userManager.IsInRoleAsync(admin, AdminRole))
+                await userManager.AddToRoleAsync(admin, AdminRole);
+        }
+    }
+}
